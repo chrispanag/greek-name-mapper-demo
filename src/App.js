bd65b8cba9fd7bc4af4efccf7c738eab1a258472
Nikos Polykandriotis <nikospolykandriotis@Nikoss-MacBook-Pro.local>
2018-09-10 18:33:57 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import data from './processed.json';
+import { Button, Input } from 'semantic-ui-react';
+
+const Mapper = require('greek-names-mapper');
+const klitiki = require('greek-name-klitiki');
+
+class App extends Component {
+  
+  state = {
+    name: '',
+    transformed: '',
+    result: '',
+    mapper: new Mapper(data)
+  }
+
+  onClick = () => {
+    const { mapper, name } = this.state;
+    if(!name) {
+      this.setState({ result: 'Πρεπει να βάλεις όνομα'});
+      return
+    }
+    const result = mapper.match(name);
+    const transformed = klitiki(result);
+    this.setState({ result, transformed }); 
+  }
+  
+  render() {
+    const { name, result, transformed } = this.state;
+    return (
+      <div className="App">
+          <h2>Απο Greeklish σε Ελληνικά</h2>
+          <Input type="text" value={name} onChange={(event) => this.setState({ name: event.target.value })}/>
+          <Button style={{ marginLeft: '10px' }}onClick={this.onClick}>GO</Button>
+          <p style={{ paddingTop: '30px', fontSize: '22px'}}>Μετάφραση : {result}</p>
+          <p style={{ paddingTop: '10px', fontSize: '22px'}}>Κλητική : {transformed}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
